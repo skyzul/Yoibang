9bd19fd0cf86ec04e1605af25b84c16dd3673bc9
zulgezet <89974542+ZulGanzy@users.noreply.github.com>
2021-10-17 15:23:10 +0700

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -1,9 +1,6 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
-Base : https://github.com/Nurutomo/wabot-aq
-Game - adventure : https://github.com/botstylee/botstylee/
-Button cmd : https://github.com/ariffb25/stikerinbot
-Keseluruhan fitur bot : https://github.com/andyjavadams/botwa
+https://github.com/Nurutomo/wabot-aq
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*ini bang*', 'status@broadcast')

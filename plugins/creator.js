ae67c14d65555b0e83b392debcf87e98b478e48d
zulgezet <89974542+ZulGanzy@users.noreply.github.com>
2021-10-28 11:11:35 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,9 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6285795431803', 'Andy', m)
   conn.sendContact(m.chat, '6289528232401', 'Zul', m)
-  m.reply('wa.me/6289528232401')
+conn.reply(m.chat, `@${global.owner[0]}
+`.trim(), m, { contextIfo: { mentionedJid: [global.owner[0] + '@s.whatsapp.net'] } })
+conn.reply(m.chat, `wa.me/6289528232401`, m)
 }
 
 handler.command = /^owner|creator$/i

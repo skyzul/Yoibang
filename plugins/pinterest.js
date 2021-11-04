00926fcbd89034a543e0f69e4145363c25e04c80
zulgezet <89974542+ZulGanzy@users.noreply.github.com>
2021-11-04 12:09:51 +0700

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -1,18 +1,26 @@
+let { Presence } = require('@adiwajshing/baileys')
 let fetch = require('node-fetch')
-let handler = async(m, { conn, text }) => {
-  let res = await fetch(global.API('https://fdciabdul.tech', '/api/pinterest', {
-    keyword : encodeURI(text)
-  }))
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  let pint = json[Math.floor(Math.random() * json.length)];
-  conn.sendFile(m.chat, pint, '', `
-*PINTEREST BY © ANDY-BOTZ*
-*Hasil Pencarian ${text} Ditemukan*
-`.trim(), m)
+const { Pinterest } = require('../lib/pinterest.js')
+let handler  = async (m, { conn, args, usedPrefix, command, text}) => {
+let txt = args.join` `
+let [query,aha]= text.split(/[&|.]/i)
+    	  if (!query) return conn.reply(m.chat, 'mau nyari apa?\ngunakan command seperti ini\n*contoh*\n'+`${usedPrefix+command} keqing|1`, m)
+	  if (!aha) return conn.reply(m.chat, 'Mau nyari berapa gambar?\ngunakan command seperti ini\n*contoh*\n'+`${usedPrefix+command} keqing|1`, m)
+          if (isNaN(aha)) return conn.reply(m.chat,  'Harus menggunakan angka',m)
+ if (aha < 1 || aha > 4) {
+      throw `[permintaan error]\n untuk menggunakan fitur ini minimal request 1 dan maksimal request 4`
+    }
+let res = await Pinterest(query)
+for (let i = 0; i < `${aha}`; i++) {
+conn.sendFile(m.chat, res[Math.floor(Math.random() * res.length)],`${query}`+'.jpg', 'imni bang', m, false, { thumbnail: Buffer.alloc(0) })
+await delay(2500)
 }
-handler.help = ['pinterest <keyword>']
+}
+ 
+handler.help = ['pinterest'].map(v => v + ' [teks]|[jumlah]')
 handler.tags = ['internet']
 handler.command = /^(pinterest)$/i
-//MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS dan Ftwrr
+
 module.exports = handler
+
+const delay = time => new Promise(res => setTimeout(res, time))

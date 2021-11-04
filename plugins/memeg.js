482ff10ad0fa38b628df98e7bf08c940c832ed53
zulgezet <89974542+ZulGanzy@users.noreply.github.com>
2021-11-04 11:35:56 +0700

    Update memeg.js

--- plugins/memeg.js
+++ plugins/memeg.js
@@ -1,24 +1,20 @@
 const uploadFile = require('../lib/uploadFile')
 const uploadImage = require('../lib/uploadImage')
-let handler = async (m, { conn, text }) => {
-  let [t1, t2] = text.split `|`
-  if (!t1) throw 'No Text'
-  if (!t2) {
-    t2 = t1
-    t1 = ''
-  }
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `uhm.. teksnya mana?\n\n${usedPrefix + command} <teks atas>|<teks bawah>`
+  let [t1, t2] = text.split`|`
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
   if (!mime) throw `Unknown Mimetype`
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak didukung`
   let img = await q.download()
   let link = await uploadImage(img).catch(e => uploadFile(img))
-  conn.sendFile(m.chat, global.API('https://api.memegen.link', `/images/custom/${encodeURIComponent(t1)}/${encodeURIComponent(t2)}.png`, {
+  conn.sendFile(m.chat, global.API('https://api.memegen.link', `/images/custom/${encodeURIComponent(t1 ? t1 : '_')}/${encodeURIComponent(t2 ? t2 : '_')}.png`, {
     background: link
-  }), 'meme.png', `Nih :|`, m)
+  }), 'meme.png', '© zullsaha', m)
 }
-handler.help = ['memeg'].map(v => v + '<apa|apa>')
+handler.help = ['textmaker'].map(v => v + ' <teks atas>|<teks bawah>')
 handler.tags = ['tools']
-handler.command = /^(memeg|textmaker)$/i
+handler.command = /^(textmaker|memeg)$/i
 
 module.exports = handler

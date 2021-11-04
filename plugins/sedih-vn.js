a1ecb53173425dad08ea6addd3e1bc9bfb3a3dad
zulgezet <89974542+ZulGanzy@users.noreply.github.com>
2021-11-04 12:25:27 +0700

    Create sedih-vn.js

---
+++ plugins/sedih-vn.js
@@ -0,0 +1,17 @@
+//tadinya iseng, eb malah work
+let fs = require("fs")
+const { MessageType,Presence } = require('@adiwajshing/baileys')
+const f = {
+	 key:
+	 { fromMe: false,
+	 participant: `0@s.whatsapp.net`, ...(m.chat ? 
+	 { remoteJid: "status@broadcast" } : {}) },
+	 message: { "locationMessage": { "title":"jakarta","h": `hemlo`, 'jpegThumbnail': fs.readFileSync('./src/ceue.jpg')}}
+	}
+
+conn.updatePresence(m.chat, Presence.recording)
+conn.sendFile(m.chat, 'src/sad.opus', 'tts.opus', null, f, true)
+}
+handler.customPrefix = /^(sedih|Sedih|semdih|Semdih|😭|😢|😔|😓)/i
+handler.command = new RegExp
+module.exports = handler
